49731c98d7f8e7ce2b5af104b69bc60273fe916e
Maiqel1 <66220194+Maiqel1@users.noreply.github.com>
2020-07-16 10:00:28 +0100

    Add files via upload

--- includes/getnotified.inc.php
+++ includes/getnotified.inc.php
@@ -7,12 +7,28 @@
 
     $email = htmlspecialchars($email);
 
-    $result = mysqli_num_rows(mysqli_query($conn, "SELECT 1 FROM diggit WHERE email = '$email';"));
+    /*$result = mysqli_num_rows(mysqli_query($conn, "SELECT 1 FROM diggit WHERE email = '$email';"));
 
     $email = $_POST['email'];
 
     $sql = "INSERT INTO diggit (email) VALUES (' $email');";
-    mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql);*/
+
+    mysqli_select_db($conn, "db name") or die("no db found");
+
+    if(isset($_POST['submit'])){
+      $email = $_POST['email'];
+
+      $query = "INSERT INTO diggit (email) VALUES('$email')";
+      mysqli_query($conn, $query);
+      /*if(mysqli_query($conn, $query)){
+        //echo "<h1>SUCCESS<h1>";
+        header("location: success.html");
+      } else {
+        // echo "<h1>FAILED<h1>";
+        header("location: failed.html");
+      }
+    }*/
 
 
 
