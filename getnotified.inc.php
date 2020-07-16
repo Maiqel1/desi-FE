4421dcbda68cf1b94fe29d0bb1d6f4904601ddd5
Maiqel1 <66220194+Maiqel1@users.noreply.github.com>
2020-07-16 14:51:10 +0100

    Add files via upload

--- getnotified.inc.php
+++ getnotified.inc.php
@@ -8,15 +8,15 @@
 
       $sql = "INSERT INTO diggit (email) VALUES('$email')";
       header("location: ../index.html");
-      /*if(mysqli_query($conn, $query)){
-        //echo "<h1>SUCCESS<h1>";
-        header("location: success.html");
-      } else {
-        // echo "<h1>FAILED<h1>";
-        header("location: failed.html");
-      }
-    }*/
+
+    }
 
 
-
+    /*if(mysqli_query($conn, $query)){
+      //echo "<h1>SUCCESS<h1>";
+      header("location: success.html");
+    } else {
+      // echo "<h1>FAILED<h1>";
+      header("location: failed.html");
+    }
 ?>
