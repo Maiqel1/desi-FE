648ecaeed22b7398ba2e9ec419f1a563e28f66a1
Maiqel1 <66220194+Maiqel1@users.noreply.github.com>
2020-07-17 16:33:20 +0100

    Update getnotified.inc.php

--- getnotified.inc.php
+++ getnotified.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-include './database.php';
+include 'dbh.inc.php';
 
 $email = trim($_POST['email']); 
 
@@ -10,32 +10,32 @@ $email = htmlspecialchars($email);
 
 $result = mysqli_num_rows(mysqli_query($conn, "SELECT 1 FROM diggit2 WHERE email = '$email';"));
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Check if email is valid
 {
     $signal = 'bad';
     $message = 'Please enter a valid email';
-} elseif ($result > 0) 
+} elseif ($result > 0) // Check if email exists already
 {
     $signal = 'bad';
     $message = 'Email already exists';
-} else { 
+} else { // All is good.. Lets save the email
     $sql = "INSERT INTO diggit2 (email) VALUES ('$email')";
     
     if (mysqli_query($conn, $sql)) {
         $signal = 'ok';
-        $message = 'Your Email has been registered successfully';
+        $message = 'Email registered successfully';
     } else {
         $signal = 'bad';
         $message = "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 }
 
-mysqli_close($conn); 
+mysqli_close($conn); // Close connection
 
 $data = array(
     'signal' => $signal,
     'msg' => $message
 );
 
-
+// Return data
 echo json_encode($data);
